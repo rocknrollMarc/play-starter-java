d0f18fc9cf1b2a3c554f3282ef7c02045618e928
Marc Bluemner <Marc.Bluemner@mspag.com>
2014-05-20 08:37:11 +0200

    added action

---
+++ app/controllers/Entries.java
@@ -0,0 +1,26 @@
+package controllers;
+
+import play.mvc.*;
+
+    public class Entries extends Controller {
+
+        public  static Result list(String filter) {
+            return TODO;
+        }
+
+        public static Result remove(long id) {
+            return TODO;
+        }
+
+        public static Result add() {
+            return TODO;
+        }
+
+        public static  Result edit(long id) {
+            return TODO;
+        }
+
+        public static Result save() {
+            return TODO;
+        }
+    }
