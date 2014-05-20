6856185d464dc6644ce086f0050c287f2d6b3c5b
Marc Bluemner <Marc.Bluemner@mspag.com>
2014-05-20 11:10:10 +0200

    fixed up

--- app/models/Entries.java
+++ app/models/Entries.java
@@ -1,3 +1,15 @@
+/*
+ *
+ * Instant Play Framework Starter
+ * Phone Book Example
+ *   ______  ___                             ___                     ___ __
+ *  /   _  \/  /__  ______  ______  ______  /  /__  ______  ______  /  /  /
+ * /   ____/   _  \/   _  \/   _  \/   ___\/   _  \/   _  \/   _  \/      \
+ * \__/    \__/___/\______/\__/___/\______/\______/\______/\______/\__/___/
+ *
+ * Published under MIT License. Copyright (c) 2013 Daniel Dietrich, Packt Publishing
+ *
+ */
 package models;
 
 import static play.libs.Scala.toSeq;
@@ -10,37 +22,33 @@ import scala.collection.Seq;
 
 public class Entries {
 
-  private static Map<Long, Entry> entries =
-    new ConcurrentHashMap<Long, Entry>();
+	private static Map<Long, Entry> entries = new ConcurrentHashMap<Long, Entry>();
 
-    private static AtomicLong uuid = new AtomicLong();
+	private static AtomicLong uuid = new AtomicLong();
 
-    public static void delete(long id) {
-        entries.remove(id);
-    }
+	public static void delete(long id) {
+		entries.remove(id);
+	}
 
-    public static Entry findById(long id) {
-        return entries.get(id);
-    }
+	public static Entry findById(long id) {
+		return entries.get(id);
+	}
+	
+	public static Seq<Entry> findByName(String filter) {
+		List<Entry> result = new ArrayList<Entry>();
+		for (Entry entry : entries.values()) {
+			if (entry.name.toLowerCase().contains(filter.toLowerCase())) {
+				result.add(entry);
+			}
+		}
+		return toSeq(result);
+	}
 
-// Using Seq , Scala collections library !! Fucking Cool, mixing scala with java code!!
-    public static Seq<Entry> findByName(String filter) {
-        List<Entry> result = new ArrayList<Entry>();
-        for (Entry entry : entries.values()) {
-            if (entry.name.toLowerCase().contains(filter.toLowerCase())) {
-                result.add(entry);
-        }
-    }
-
-    return toSeq(result);
-}
-
-
-public static void save(Entry entry) {
-    if (entry.id ==null) {
-        entry.id = uuid.incrementAndGet();
-    }
-    entries.put(entry.id, entry);
-    }
+	public static void save(Entry entry) {
+		if (entry.id == null) {
+			entry.id = uuid.incrementAndGet();
+		}
+		entries.put(entry.id, entry);
+	}
 
 }
